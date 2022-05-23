45fca2d89866082b9d05270c63ec6c97b292f3b6
Justin M. Jones <46495106+JustMJ@users.noreply.github.com>
2022-05-23 18:22:35 -0400

    making ID retrieval more flexible

--- R/create_adj_matrix.R
+++ R/create_adj_matrix.R
@@ -3,29 +3,23 @@
 #' This function turns edgelists into Adjacency Matrices.
 #'
 #' @export
-create_adj_matrix<- function(edge_data) {
-
-  edge <- edge_data %>%
-    filter(to %in% panel$ExternalDataReference) %>%
-    filter(from %in% panel$ExternalDataReference) %>%
-    mutate(tie = 1) %>% # This filter makes the matrices symmetric
-    mutate_if(~ any(is.character(.)),~as.numeric(.))
-
-  empty_edge<- expand_grid(x = panel$ExternalDataReference,y = panel$ExternalDataReference)
-
-  empty_edge %<>%
-    mutate(tie = 0)
-
-  empty_net<- anti_join(empty_edge,edge,by=c("x"="from","y"="to"))
-
-  temp <- edge %>%
-    full_join(empty_net,by=c("from"="x","to"="y","tie"="tie")) %>%
-    mutate(tie = case_when(from == to ~ NA_real_, TRUE ~ tie))
-
-  adj<- temp %>%
-    arrange(from,to) %>%
-    pivot_wider(names_from=to,values_from=tie) %>%
-    column_to_rownames("from")
-
+cr_edge<- function (edge_data,panel) 
+{
+  edge <- edge_data %>% 
+    mutate_all(as.numeric) %>% 
+    filter(to %in% pull(panel,which(grepl("external",colnames(panel),ignore.case = T))))  %>%
+    mutate(tie = 1) %>% 
+    mutate_if(~any(is.character(.)), ~as.numeric(.))
+  
+  empty_edge <- expand_grid(x =pull(panel,which(grepl("external",colnames(panel),ignore.case = T))), 
+                            y = pull(panel,which(grepl("external",colnames(panel),ignore.case = T))))
+  empty_edge %<>% mutate(tie = 0)
+  empty_net <- anti_join(empty_edge, edge, by = c(x = "from", 
+                                                  y = "to"))
+  temp <- edge %>% full_join(empty_net, by = c(from = "x", 
+                                               to = "y", tie = "tie")) %>% mutate(tie = case_when(from == 
+                                                                                                    to ~ NA_real_, TRUE ~ tie))
+  adj <- temp %>% arrange(from, to) %>% pivot_wider(names_from = to, 
+                                                    values_from = tie) %>% column_to_rownames("from")
   return(adj)
 }
